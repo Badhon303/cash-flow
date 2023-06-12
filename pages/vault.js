2dc197239027e26bde226bc2a4acfd7e8f8239e9
Badhon303 <badhon.alam303@gmail.com>
2023-06-12 18:18:28 +0600

    vault ip changed

--- pages/vault.js
+++ pages/vault.js
@@ -3,7 +3,7 @@ const vault = () => {
     <div className="full">
       <iframe
         className="frame"
-        src="https://app.powerbi.com/reportEmbed?reportId=6bba8f0a-39ea-4b9b-8fb6-f43a16cb280a&autoAuth=true&ctid=147d207c-cfb1-4dda-bebc-d96c8cb48eed%22"
+        src="https://app.powerbi.com/reportEmbed?reportId=6bba8f0a-39ea-4b9b-8fb6-f43a16cb280a&autoAuth=true&ctid=147d207c-cfb1-4dda-bebc-d96c8cb48eed"
       ></iframe>
     </div>
   )
